d6fa0ee18883e82fe073b510822df561c262f4ec
tjado <tjado@maecke.de>
2016-09-21 15:52:29 +0200

    Fixed redirect bug

--- exec.sh
+++ exec.sh
@@ -2,4 +2,4 @@
 
 SCRIPT_DIR=`dirname $0`
 
-/usr/bin/nohup ${SCRIPT_DIR}/tracer.sh > ${SCRIPT_DIR}/log-error.log 2>&1 &
+/usr/bin/nohup ${SCRIPT_DIR}/tracer.sh >> ${SCRIPT_DIR}/log-error.log 2>&1 &
