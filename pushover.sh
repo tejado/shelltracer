e40b2679a4a9c92279cdf9f2ef7f8658e66b4578
Andreas Eisenreich <andi@nanuc.de>
2016-12-28 02:02:18 +0100

    renamed - inital commit

--- pushover.sh
+++ pushover.sh
@@ -1,11 +1,16 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-SCRIPT_DIR=`dirname $0`
-. "${SCRIPT_DIR}/config"
+SCRIPT_DIR=$(dirname $0)
+if [ -f ${SCRIPT_DIR}/config ];then
+	. "${SCRIPT_DIR}/config"
+else
+	echo "ERROR: Not able to load configuration \"${SCRIPT_DIR}/config\""
+	exit 1
+fi
 
 PUSHOVER_TITLE="$1"
 PUSHOVER_MESSAGE="$2"
 PUSHOVER_URL="https://api.pushover.net/1/messages.json"
 
-${CURL} -s -F "token=${PUSHOVER_TOKEN_APP}" -F "user=${PUSHOVER_TOKEN_USER}" -F "title=${PUSHOVER_TITLE}" -F "message=${PUSHOVER_MESSAGE}" ${PUSHOVER_URL} >> ${LOGFILE} 2>&1
+/usr/bin/env curl -s -F "token=${PUSHOVER_TOKEN_APP}" -F "user=${PUSHOVER_TOKEN_USER}" -F "title=${PUSHOVER_TITLE}" -F "message=${PUSHOVER_MESSAGE}" ${PUSHOVER_URL} >> ${LOGFILE} 2>&1
 echo >> ${LOGFILE}
