d1c63a8766ffa9473277ef570ef6f2d0bed81b9b
Andreas Eisenreich <andi@nanuc.de>
2016-12-28 02:17:03 +0100

    check pushover api call return code

--- pushover.sh
+++ pushover.sh
@@ -13,4 +13,9 @@ PUSHOVER_MESSAGE="$2"
 PUSHOVER_URL="https://api.pushover.net/1/messages.json"
 
 /usr/bin/env curl -s -F "token=${PUSHOVER_TOKEN_APP}" -F "user=${PUSHOVER_TOKEN_USER}" -F "title=${PUSHOVER_TITLE}" -F "message=${PUSHOVER_MESSAGE}" ${PUSHOVER_URL} >> ${LOGFILE} 2>&1
+RC=$?
+if [ ! ${RC} -eq 0 ];then
+	echo "ERROR: Not able to push pushover message" >>${LOGFILE}
+	exit 1
+fi
 echo >> ${LOGFILE}
