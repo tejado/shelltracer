6d8e0510f9f05fa09cf04b767fb41715a5d39737
Andreas Eisenreich <andreas.eisenreich@posteo.net>
2017-01-31 21:44:58 +0100

    obsolete

--- notify.sh
+++ notify.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 SCRIPT_DIR=`dirname $0`
 . "${SCRIPT_DIR}/config"
@@ -7,5 +7,5 @@ PUSHOVER_TITLE="$1"
 PUSHOVER_MESSAGE="$2"
 PUSHOVER_URL="https://api.pushover.net/1/messages.json"
 
-${CURL} -s -F "token=${PUSHOVER_TOKEN_APP}" -F "user=${PUSHOVER_TOKEN_USER}" -F "title=${PUSHOVER_TITLE}" -F "message=${PUSHOVER_MESSAGE}" ${PUSHOVER_URL} >> ${LOGFILE} 2>&1
+curl -s -F "token=${PUSHOVER_TOKEN_APP}" -F "user=${PUSHOVER_TOKEN_USER}" -F "title=${PUSHOVER_TITLE}" -F "message=${PUSHOVER_MESSAGE}" ${PUSHOVER_URL} >> ${LOGFILE} 2>&1
 echo >> ${LOGFILE}
