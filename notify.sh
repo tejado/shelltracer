b47fb602470c74f63ff08cdb1cea1852dd44d4ec
imquibie <johannfischer@protonmail.com>
2019-12-28 16:55:14 +0100

    Add telegram-cli integration

--- notify.sh
+++ notify.sh
@@ -8,4 +8,9 @@ PUSHOVER_MESSAGE="$2"
 PUSHOVER_URL="https://api.pushover.net/1/messages.json"
 
 ${CURL} -s -F "token=${PUSHOVER_TOKEN_APP}" -F "user=${PUSHOVER_TOKEN_USER}" -F "title=${PUSHOVER_TITLE}" -F "message=${PUSHOVER_MESSAGE}" ${PUSHOVER_URL} >> ${LOGFILE} 2>&1
+
+if [ $USE_TELEGRAM = "YES" ]; then
+    telegram-cli -W -e "msg $TELEGRAM_ACCOUNT_NAME $PUSHOVER_MESSAGE" >> ${LOGFILE} 2>&1
+fi
+
 echo >> ${LOGFILE}
